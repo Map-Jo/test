636019712e48486542319dafa1e231650cc80e12
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-10 22:40:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,4 +7,13 @@ import plotly.express as px
 
 st.write('Do you need recommend?')
 
-df = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv")
+url = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv")
+
+
+@st.cache
+def load_data(nrows):
+      data = pd.read_csv(url, nrows=nrows)
+      lowercase = lambda x: str(x).lower()
+      data.rename(lowercase, axis='columns', inplace=True)
+      data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+      return data
