b04767cb46507fe53ea0aa60a854684db36f39b7
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-10 22:30:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,3 @@ import plotly.express as px
 st.write('Do you need recommend?')
 
 df = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv")
-fig = px.bar(df, x="지역",y=["길단위 유동인구"])
