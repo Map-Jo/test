eff802192a43fc6361f695e81028f359b0d292f1
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-10 22:20:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-
 
 st.write('Do you need recommend?')
 
 st.slidebar.header("Menu")
-
-region = st.sidebar.selectbox("자치구",['종로구', '중구', '용산구', '성동구', '광진구', '동대문구', '중랑구', '성북구',
-       '강북구', '도봉구', '노원구', '은평구', '서대문구', '마포구', '양천구', '강서구', '구로구',
-       '금천구', '영등포구', '동작구', '관악구', '서초구', '강남구', '송파구', '강동구'])
-                             
