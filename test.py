1f13b64635f4ccef2990b9e5515e208ae03a068a
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-10 22:09:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,49 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
+# 윈도우 : "Malgun Gothic"
+# 맥 : "AppleGothic"
+def get_font_family():
+    """
+    시스템 환경에 따른 기본 폰트명을 반환하는 함수
+    """
+    import platform
+    system_name = platform.system()
+    # colab 사용자는 system_name이 'Linux'로 확인
+
+    if system_name == "Darwin" :
+        font_family = "AppleGothic"
+    elif system_name == "Windows":
+        font_family = "Malgun Gothic"
+    else:
+        !apt-get install fonts-nanum -qq  > /dev/null
+        !fc-cache -fv
+
+        import matplotlib as mpl
+        mpl.font_manager._rebuild()
+        findfont = mpl.font_manager.fontManager.findfont
+        mpl.font_manager.findfont = findfont
+        mpl.backends.backend_agg.findfont = findfont
+        
+        font_family = "NanumBarunGothic"
+    return font_family
+
+
+plt.style.use("seaborn-whitegrid")
+
+# 폰트설정
+plt.rc("font", family=get_font_family())
+# 마이너스폰트 설정
+plt.rc("axes", unicode_minus=False)
+
+# 그래프에 retina display 적용
+from IPython.display import set_matplotlib_formats
+
+%config InlineBackend.figure_format = 'retina'
+
+
+
+
 st.write('어느 자치구에 개업을 생각 중이신가요?')
 
 st.slidebar.header("Menu")
