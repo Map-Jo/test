e45ba51b46a3559b6c8149de5bc8fb23b5a10b69
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-10 22:15:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,54 +6,12 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-# 윈도우 : "Malgun Gothic"
-# 맥 : "AppleGothic"
-def get_font_family():
-    """
-    시스템 환경에 따른 기본 폰트명을 반환하는 함수
-    """
-    import platform
-    system_name = platform.system()
-    # colab 사용자는 system_name이 'Linux'로 확인
 
-    if system_name == "Darwin" :
-        font_family = "AppleGothic"
-    elif system_name == "Windows":
-        font_family = "Malgun Gothic"
-    else:
-        !apt-get install fonts-nanum -qq  > /dev/null
-        !fc-cache -fv
-
-        import matplotlib as mpl
-        mpl.font_manager._rebuild()
-        findfont = mpl.font_manager.fontManager.findfont
-        mpl.font_manager.findfont = findfont
-        mpl.backends.backend_agg.findfont = findfont
-        
-        font_family = "NanumBarunGothic"
-    return font_family
-
-
-plt.style.use("seaborn-whitegrid")
-
-# 폰트설정
-plt.rc("font", family=get_font_family())
-# 마이너스폰트 설정
-plt.rc("axes", unicode_minus=False)
-
-# 그래프에 retina display 적용
-from IPython.display import set_matplotlib_formats
-
-%config InlineBackend.figure_format = 'retina'
-
-
-
-
-st.write('어느 자치구에 개업을 생각 중이신가요?')
+st.write('Do you need recommend?')
 
 st.slidebar.header("Menu")
 
 region = st.sidebar.selectbox("자치구",['종로구', '중구', '용산구', '성동구', '광진구', '동대문구', '중랑구', '성북구',
        '강북구', '도봉구', '노원구', '은평구', '서대문구', '마포구', '양천구', '강서구', '구로구',
-       '금천구', '영등포구', '동작구', '관악구', '서초구', '강남구', '송파구', '강동구']
+       '금천구', '영등포구', '동작구', '관악구', '서초구', '강남구', '송파구', '강동구'])
                              
