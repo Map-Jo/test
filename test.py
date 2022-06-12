bd9f7ac093e6de0b86407cd6a703c7ddbd680c0f
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-12 17:46:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -89,9 +89,9 @@ for i in object_list:
 df['지역'] = df['지역'].str.strip()
 df_a = df[df['지역'] != '서울시 전체']
 plt.figure(figsize=(12, 10))
-fig1 = sns.barplot(data=df_a.sort_values('전체 점포수'),x='전체 점포수',y='지역', ci=None)
+sns.barplot(data=df_a.sort_values('전체 점포수'),x='전체 점포수',y='지역', ci=None)
 _ = plt.title('서울시 구별 21년도 전체 점포수')
-st.pyplot(fig1)
+st.pyplot()
 
 
 
