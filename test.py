fded8baee44fd5a87583c22e8f0b1ae7f9ad3915
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-12 21:10:05 +0900

    Update test.py

--- test.py
+++ test.py
@@ -196,12 +196,9 @@ elif input_reg == "강동구":
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)
-
+    st.subheader('Raw data')
     st.write(a)
 
-    st.write(b)
-
-    st.write(c)
 
 df_c = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv")
 object_list=['전체 점포수','프랜차이즈 점포수','일반 점포수','길단위 유동인구', '개업수', '폐업수']
