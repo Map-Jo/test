20d520fa74b7f1b3f7a83fed198a76254a7cbca7
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-13 01:34:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ def total_graph(gu):
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title("서울시 내에 창업을 계획중이신가요?")
+st.title("서울에 창업을 계획중이신가요?")
 input_reg = st.text_input(label="지역명 ex)종로구", value="지역명을 입력해주세요.")
 
 
