3ccea567ccd382dab38f1b587a0c6e0e00339282
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-12 19:58:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -339,10 +339,10 @@ st.pyplot()
 geo_path = pd.read_json("https://raw.githubusercontent.com/Map-Jo/test/main/seoul_municipalities_geo_simple%20(1).json")
 
 geo_json = json.load(open(geo_path, encoding="utf-8"))
-df_pop = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv')  #인구수 파일이어서 변수명을 popular의 Pop으로
+df_pop = pd.read_csv("https://raw.githubusercontent.com/Map-Jo/test/main/%EC%9D%B8%EA%B5%AC_%EC%A0%90%ED%8F%AC_%EA%B0%9C%ED%8F%90%EC%97%85_%ED%86%B5%ED%95%A9_2021%20(2).csv')
 df_pop
 df_popular= df_pop.groupby("지역")["직장 인구"].sum().reset_index()
-df_popular['지역']=df_popular['지역'].str.strip() #인구수 csv 파일에서 지역명앞에 한칸 띄어져있어서 strip해줌
+df_popular['지역']=df_popular['지역'].str.strip()
 df_popular #choropleth 그리기 위해서 필요한 데이터
 
 
